a87b5d768eca008b9e64f6d0c0192722fea1480f
Kseniya Naumycheva <naumycheva@naumycheva-lx.nixsolutions.com>
2019-06-25 16:41:28 +0300

    25.06 fix quadraric + fix method names

--- quadratic_equation.rb
+++ quadratic_equation.rb
@@ -3,31 +3,32 @@
 def quadratic_equation_calculation
   variables = input_variables
   discriminant = calculate_discriminant(variables)
-  put_answer discriminant
+  puts discriminant.inspect
+  put_answer discriminant, variables
 end
 
-def put_answer(discriminant)
-  negative_discriminant? discriminant
-  positive_discriminant? discriminant
-  zero_discriminant? discriminant
+def put_answer(discriminant, variables)
+  negative_discriminant?(discriminant) ||
+    positive_discriminant?(discriminant, variables) ||
+    zero_discriminant?(discriminant, variables)
 end
 
 def negative_discriminant?(discriminant)
-  message = "It's discriminant - #{discriminant}.
+  message = "It's discriminant: #{discriminant}.
   There are no roots"
   puts message if discriminant.negative?
 end
 
-def positive_discriminant?(discriminant)
-  message =  "It's discriminant - #{discriminant}.
-  It's first root - #{calculate_first_root discriminant variables},
-  it's second root #{calculate_second_root discriminant variables}."
+def positive_discriminant?(discriminant, variables)
+  message =  "It's discriminant: #{discriminant}.
+  It's first root: #{calculate_first_root discriminant, variables},
+  it's second root: #{calculate_second_root discriminant, variables}."
   puts message if discriminant.positive?
 end
 
-def zero_discriminant?(discriminant)
-  message = "It's discriminant - #{discriminant}.
-  It's first root - #{calculate_one_root variables}"
+def zero_discriminant?(discriminant, variables)
+  message = "It's discriminant: #{discriminant}.
+  It's first root: #{calculate_one_root variables}"
   puts message if discriminant.zero?
 end
 
@@ -40,8 +41,7 @@ def input_variables
   a = gets.chomp.to_f
   b = gets.chomp.to_f
   c = gets.chomp.to_f
-  variables = []
-  variables.push(a.to_f, b.to_f, c.to_f)
+  [a, b, c]
 end
 
 def calculate_first_root(discriminant, variables)
